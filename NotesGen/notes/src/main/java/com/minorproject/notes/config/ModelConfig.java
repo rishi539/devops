114a7470efb7307cc373e6f4d5d714ebc6eaadf7
Ubuntu <ubuntu@ip-10-0-130-255.ec2.internal>
2026-05-15 10:47:12 +0000

    no nignx

--- NotesGen/notes/src/main/java/com/minorproject/notes/config/ModelConfig.java
+++ NotesGen/notes/src/main/java/com/minorproject/notes/config/ModelConfig.java
@@ -1,9 +1,6 @@
 package com.minorproject.notes.config;
 
 import com.minorproject.notes.service.NotesService;
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.google.genai.GoogleGenAiChatModel;
-import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -12,26 +9,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ModelConfig {
+
     @Bean
-    @Scope("prototype") // new instance each time
+    @Scope("prototype")
     public NotesService notesService() {
         return new NotesService();
     }
-//    @Bean
-//    public ChatClient createChatClient(OllamaChatModel ollamaChatModel) {
-//        return ChatClient.create(ollamaChatModel);
-//    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173",
-                                "http://localhost:5174",
-                                "https://5nqsc8ft-5173.inc1.devtunnels.ms/",
-                                "https://8sm32xfg-5173.inc1.devtunnels.ms/")// ✅ your React dev server
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
